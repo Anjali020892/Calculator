6fa9117d9208d1a6540576024837a2149c800602
Anjali020892 <71452344+Anjali020892@users.noreply.github.com>
2020-09-29 10:03:19 -0400

    Nuint Test

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CalculatorApp;
+using NUnit.Framework;
+
+namespace NUnit_test
+{
+    [TestFixture]
+    class NUnit_test
+    {
+       
+
+            [Test]
+            public void DummyPassTest()
+            {
+                Assert.Pass();
+            }
+
+            [Test]
+            public void DummyFailTest()
+            {
+                Assert.Fail();
+            }
+
+        
+    }
+}
